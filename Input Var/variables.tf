35b42c56c99627dad31250789927859f8d9b1d13
Ezekiel Ayanda <ayandaezekiel@gmail.com>
2021-08-06 08:32:19 +0000

    New variable usage learnt

---
+++ Input Var/variables.tf
@@ -0,0 +1,15 @@
+variable "filename" {
+  default = "~/desktop/vartest.txt"
+}
+variable "content" {
+  default = "my favorite pet is Miles!"
+}
+variable "prefix" {
+  default = "Mrs"
+}
+variable "separator" {
+  default = "."
+}
+variable "length" {
+  default = "2"
+}
