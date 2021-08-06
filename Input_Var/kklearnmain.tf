5956680bcf3b224411ee03f1ad971863c6754386
Ezekiel Ayanda <ayandaezekiel@gmail.com>
2021-08-06 11:51:10 +0000

    some adjustment and key Gen flow

---
+++ Input_Var/kklearnmain.tf
@@ -0,0 +1,13 @@
+resource "local_file" "pet" {
+   filename = var.filename
+   content = var.content
+  
+
+}
+
+resource "random_pet" "my_pet" {
+   prefix = var.prefix
+   separator = var.separator
+   length = var.length
+
+}
---
+++ Input_Var/variables.tf
@@ -0,0 +1,15 @@
+variable "filename" {
+  default = "~/desktop/vartest.txt"
+}
+variable "content" {
+  default = "my favorite pet is mr white"
+}
+variable "prefix" {
+  default = "Mrs"
+}
+variable "separator" {
+  default = "."
+}
+variable "length" {
+  default = "2"
+}
