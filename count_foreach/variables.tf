2fbd117fd1df9ca7ccbb79eeeb04097508659439
Ezekiel Ayanda <ayandaezekiel@gmail.com>
2021-08-09 09:51:20 +0000

    Using Count Argument

---
+++ count_foreach/variables.tf
@@ -0,0 +1,8 @@
+variable "filename" {
+  default = [
+      "deployedfiles/goose.txt",
+      "deployedfiles/puppy.txt",
+      "deployedfiles/donkey.html",
+      "deployedfiles/peacock.json",
+  ]
+}
